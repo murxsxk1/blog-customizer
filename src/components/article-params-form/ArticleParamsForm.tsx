4127b514569a1b7e5acc85fefb80e4c9849807eb
Danila <novash.danila@yandex.by>
2025-07-07 11:17:47 +0300

    Version 0.1 Implemented opening of an additional panel by clicking on the arrow

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,23 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [isPanelOpen, setIsPanelOpen] = useState(false);
+
+	const handleArrowClick = () => {
+		setIsPanelOpen(!isPanelOpen);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isPanelOpen} onClick={handleArrowClick} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isPanelOpen,
+				})}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
